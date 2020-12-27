2ff2e569a78a25aa10f1d5ff7eba3205ce28d04d
Zangeore <62109634+Zangeore@users.noreply.github.com>
2020-12-27 08:50:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from .. import loader, utils
 from requests import get
+import requests
 import os
+import io
 from gtts import gTTS
 from PIL import Image, ImageDraw, ImageFont
 import random
 
 def register(cb):
-    cb(AUFmod)
+    cb(AufMod)
 
-
-class AUFmod(loader.Module):
+class AufMod(loader.Module):
 
     strings = {
-        "name": "AUFmod"
+        "name": "Auf Mod"
     }
 
     async def client_ready(self, client, db):
@@ -24,17 +25,22 @@ class AUFmod(loader.Module):
         wolf=["main.jpg", "main1.jpg", "main2.jpg", "main3.jpg", "main4.jpg"]
         url = "https://raw.githubusercontent.com/Zangeore/auf_bot/main/"
         ctt = utils.get_args_raw(message)
-        olf=random.choice(get(url+wolf))
+        olf=random.choice(wolf)
         await message.edit("Жизнь ворам, Брат!!! Цитата в процессе. ")
-        im = Image.open(url+olf)
+        im = Image.open(io.BytesIO(get(url+olf).content))
         draw = ImageDraw.Draw(im)
         # font = ImageFont.truetype(<font-file>, <font-size>)
-        font = ImageFont.truetype(get(url+"main.ttf"), 50)
+        font = ImageFont.truetype(io.BytesIO(get(url+"main.ttf").content), 50)
         # draw.text((x, y),"Sample Text",(r,g,b))
         draw.text((20, 340), ctt, (0, 0, 0), font=font)
         im.save("1.jpg")
         tts = gTTS(text=ctt, lang='ru')
         tts.save("main.mp3")
+
+        f=open(r"main.mp4","wb")
+        ufr=get(url+"main.mp4")
+        f.write(ufr.content)
+        f.close()
 
         os.system("ffmpeg -loop 1 -i 1.jpg -i main.mp3 -c:v libx265  -strict \
         experimental -b:a 192k -shortest wolf.mp4 -y")
@@ -42,14 +48,9 @@ class AUFmod(loader.Module):
 
         os.system("ffmpeg -i wolf.mp4 -acodec copy -vcodec libx265   \
         -f mpegts vid1.webm -y")
-        os.system("ffmpeg -i "+get(url+main.mp4)+" -acodec copy -vcodec libx265   \
+        os.system("ffmpeg -i main.mp4 -acodec copy -vcodec libx265   \
         -f mpegts vid2.webm -y")
         os.system('ffmpeg -i "concat:vid1.webm|vid2.webm" out.mp4 -y')
 
-
-
-
-
-
         send=open("out.mp4", "rb")
-        await message.client.send_video(message.to_id, output, reply_to=reply)
+        await message.client.send_file(message.to_id, send)
